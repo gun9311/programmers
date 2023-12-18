9ac050c7d1de1ba5e5a48558fabb66944d8dbb30
[gunryul] <[gun9311@gmail.com]>
2023-12-19 00:20:55 +0900

    runner 2nd

--- baekjoon/programmers/runner.py
+++ baekjoon/programmers/runner.py
@@ -1,12 +1,15 @@
+from collections import Counter
+
 def solution(participant, completion):
     answer = ''
-    for i in completion :
-        if i in participant :
-            participant.remove(i)
-    for i in participant :
-        answer = i
-    print (answer)
-    return answer
+    c_part = Counter(participant)
+    c_comp = Counter(completion)
+    # c_part.subtract(c_comp)
+    # answer = answer.join(c_part.elements())
+    for i in c_part :
+        if c_part[i] != c_comp[i]:    
+            print (i)
+            return i
 
 p = ["leo", "kiki", "eden"]
 c = ["eden", "kiki"]
